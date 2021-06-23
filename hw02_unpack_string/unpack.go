52e3edf65fe04801e23c7bcd6d10408b9e9af798
p.botov <p.botov@crpt.ru>
2021-06-23 14:30:29 +0300

    corrected complexity of if

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -14,45 +14,45 @@ func Unpack(str string) (string, error) {
 	var countDigits int
 	var previousValue rune
 	var foundControl bool
-	if len(str) > 0 {
-		for i, val := range str {
-			switch {
-			case unicode.IsDigit(val) && i == 0:
+	if len(str) == 0 {
+		return "", nil
+	}
+	for i, val := range str {
+		switch {
+		case unicode.IsDigit(val) && i == 0:
+			return "", ErrInvalidString
+		case unicode.IsDigit(val) && foundControl:
+			previousValue = val
+			foundControl = !foundControl
+		case unicode.IsDigit(val):
+			countDigits++
+			if countDigits > 1 {
 				return "", ErrInvalidString
-			case unicode.IsDigit(val) && foundControl:
-				previousValue = val
-				foundControl = !foundControl
-			case unicode.IsDigit(val):
-				countDigits++
-				if countDigits > 1 {
-					return "", ErrInvalidString
-				}
-				digit, _ := strconv.Atoi(string(val))
-				bstring.WriteString(strings.Repeat(string(previousValue), digit))
-				previousValue = 0
-			case unicode.IsLetter(val):
-				if previousValue != 0 {
-					bstring.WriteString(string(previousValue))
-				}
-				if foundControl {
-					foundControl = !foundControl
-				}
-				previousValue = val
-				countDigits = 0
-			case unicode.IsLetter(val) && foundControl:
-				return "", ErrInvalidString
-			case val == 92 && foundControl:
-				previousValue = val
-				foundControl = !foundControl
-			case val == 92:
+			}
+			digit, _ := strconv.Atoi(string(val))
+			bstring.WriteString(strings.Repeat(string(previousValue), digit))
+			previousValue = 0
+		case unicode.IsLetter(val):
+			if previousValue != 0 {
 				bstring.WriteString(string(previousValue))
+			}
+			if foundControl {
 				foundControl = !foundControl
 			}
-		}
-		if previousValue != 0 {
+			previousValue = val
+			countDigits = 0
+		case unicode.IsLetter(val) && foundControl:
+			return "", ErrInvalidString
+		case val == 92 && foundControl:
+			previousValue = val
+			foundControl = !foundControl
+		case val == 92:
 			bstring.WriteString(string(previousValue))
+			foundControl = !foundControl
 		}
-		return bstring.String(), nil
 	}
-	return "", nil
+	if previousValue != 0 {
+		bstring.WriteString(string(previousValue))
+	}
+	return bstring.String(), nil
 }
