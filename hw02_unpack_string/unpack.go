6fbb5e1d0e7a66257e732a0f6d6f72a0a47fa536
p.botov <p.botov@crpt.ru>
2021-06-23 14:23:54 +0300

    initial

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,59 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	var bstring strings.Builder
+	var countDigits int
+	var previousValue rune
+	var foundControl bool
+	if len(str) > 0 {
+		for i, val := range str {
+			switch {
+			case unicode.IsDigit(val) && i == 0:
+				return "", ErrInvalidString
+			case unicode.IsDigit(val) && foundControl:
+				previousValue = val
+				foundControl = !foundControl
+			case unicode.IsDigit(val):
+				countDigits++
+				if countDigits > 1 {
+					return "", ErrInvalidString
+				} else {
+					digit, _ := strconv.Atoi(string(val))
+					bstring.WriteString(strings.Repeat(string(previousValue), digit))
+					previousValue = 0
+				}
+			case unicode.IsLetter(val):
+				if previousValue != 0 {
+					bstring.WriteString(string(previousValue))
+				}
+				if foundControl {
+					foundControl = !foundControl
+				}
+				previousValue = val
+				countDigits = 0
+			case unicode.IsLetter(val) && foundControl:
+				return "", ErrInvalidString
+			case val == 92 && foundControl:
+				previousValue = val
+				foundControl = !foundControl
+			case val == 92:
+				bstring.WriteString(string(previousValue))
+				foundControl = !foundControl
+			}
+		}
+		if previousValue != 0 {
+			bstring.WriteString(string(previousValue))
+		}
+		return bstring.String(), nil
+	} else {
+		return "", nil
+	}
 }
